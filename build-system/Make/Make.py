ea71e451b41150279d4a762be6e3b99b982d8fd2
b1o <erikghabuzyan6@gmail.com>
2026-08-20 22:13:06 -0700

    Update Make.py

--- build-system/Make/Make.py
+++ build-system/Make/Make.py
@@ -510,7 +510,7 @@ def resolve_configuration(base_path, bazel_command_line: BazelCommandLine, argum
         shutil.rmtree(provisioning_path)
     os.makedirs(provisioning_path, exist_ok=True)
 
-    codesigning_data = resolve_codesigning(
+        codesigning_data = resolve_codesigning(
         arguments=arguments,
         base_path=base_path,
         build_configuration=build_configuration,
@@ -519,7 +519,11 @@ def resolve_configuration(base_path, bazel_command_line: BazelCommandLine, argum
     )
     if codesigning_data.aps_environment is None:
         print('Could not find a valid aps-environment entitlement in the provided provisioning profiles')
-        sys.exit(1)
+        print('Biogram: forcing aps_environment=development (sideload / fake-codesigning)')
+        codesigning_data = ResolvedCodesigningData(
+            aps_environment='development',
+            use_xcode_managed_codesigning=codesigning_data.use_xcode_managed_codesigning
+        )
 
     if bazel_command_line is not None:
         build_configuration.write_to_variables_file(bazel_path=bazel_command_line.bazel, use_xcode_managed_codesigning=codesigning_data.use_xcode_managed_codesigning, aps_environment=codesigning_data.aps_environment, path=configuration_repository_path + '/variables.bzl')
